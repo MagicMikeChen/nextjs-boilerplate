19535b151e37c0977184a7292eec95a68872e7e2
MikeChen-Work <>
2021-12-23 13:51:56 +0800

    feat: Upgrade tailwind to v3.0.7

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,44 @@
 module.exports = {
-    mode: 'jit',
-    purge: {
-        content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
-        options: {
-            safelist: [/^bg-/, /^text-/]
-        }
-    },
-    darkMode: 'media',
-    theme: {
-        extend: {
-            animation: {
-                'spin-reverse': 'spin 1s linear infinite reverse'
-            },
-        },
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [
-        require('@tailwindcss/forms')
+    darkMode: "class",
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}',
     ],
-}
+      theme: {
+      extend: {
+        colors:{
+          blue:{
+            950: '#042448'
+          },
+          amber:{
+            950: '#7e493a'
+          }
+        },
+        height:{
+          "h-screen-scroll": "50vh",
+        },            
+        animation: {
+            'spin-reverse': 'spin 1s linear infinite reverse'
+        },
+      },
+      container: {
+        padding: {
+          DEFAULT: '1rem',
+          sm: '1rem',
+          lg: '1rem',
+          xl: '2rem',
+          '2xl': '4rem',
+        },
+      },
+      screens: {
+        sm: '480px',
+        md: '640px',
+        lg: '960px',
+        xl: '1200px',
+        '2xl': '1600px',
+      }
+    },
+    
+    plugins: [require('@tailwindcss/forms')],
+  }
+  
